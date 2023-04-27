582cd93f6f6cb8e76955f1f95a7128ab99315bc9
Carlos Abade <carloseabade@gmail.com>
2023-04-27 16:43:46 -0300

    fix: set theme colors

--- config.def.h
+++ config.def.h
@@ -7,15 +7,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Liberation Mono:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#f2ac57";
+static const char col_navy_blue[]   = "#011126";
+static const char col_dark_red[]    = "#59121f";
+static const char col_red[]         = "#8c3041";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg          bg             border   */
+	[SchemeNorm] = { col_yellow, col_navy_blue, col_navy_blue },
+	[SchemeSel]  = { col_yellow, col_dark_red,  col_red },
 };
 
 /* tagging */
@@ -57,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_navy_blue, "-nf", col_yellow, "-sb", col_dark_red, "-sf", col_yellow, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
