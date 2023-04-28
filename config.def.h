9090cea0a614ca393a5955bd9ebb20ecce95323e
Carlos Abade <carloseabade@gmail.com>
2023-04-28 13:57:25 -0300

    feat: add keyboard shortcut for slock

--- config.def.h
+++ config.def.h
@@ -63,11 +63,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_navy_blue, "-nf", col_yellow, "-sb", col_dark_red, "-sf", col_yellow, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *slock[]    = { "slock", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
